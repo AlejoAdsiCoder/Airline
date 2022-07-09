6e0602853cad2cc739ddbbc48dbb8ab72c0f712c
AlejandroP <leho.patino@gmail.com>
2022-07-09 06:21:35 -0500

    menu mobile

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,16 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Lang } from './Lang'
+import { MenuButton } from './MenuButton';
+import styled from "styled-components";
+
+export const NavbarWrapper = styled.nav`
+  @media only screen and (max-width: 624px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    background: white;
+    position: fixed;
+    top: 10vh;
+    right: ${props => (props.open ? "0" : "-100%")};
+    left: ${props => (props.open ? "0" : "unset")};
+    width: 100%;
+    height: 90vh;
+    transition: right 0.3s linear;
+    z-index: 2;
+    top: 0;
+    justify-content: center;
+    height: auto;
+    border-radius: 10px;
+    margin: 90px auto;
+    width: 90%;
+    justify-content: center;
+
+    a {
+      margin: 12px 0;
+      color: black;
+    }
+    
+  }
+
+  @media only screen and (min-width: 624px) {
+    flex-direction: row;
+    justify-content: space-between;
+    display: flex;
+    column-gap: 15px;
+    font-family: 'RobotoRegular';
+    position: unset;
+    height: unset;
+    
+  }
+`;
 
 export const Navbar = () => {
+  const [open, setOpen] = useState(false)
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
     <div className='head__functions'>
         <Lang />
-        <nav className='head__navbar'>
+        <MenuButton open={open} handleClick={handleClick} />
+        <NavbarWrapper open={open} className='head__navbar'>
             <a className='head__link' href='#'>Home</a>
             <a className='head__link' href='#'>About</a>
             <a className='head__link' href='#'>Destinations</a>
             <a className='head__link' href='#'>Contact</a>
-        </nav>
+        </NavbarWrapper>
+        
     </div>
   )
 }
